d1dc250a208c171c843ee5144929fa181bdaf2e6
DeShe <woodsdeshe94@gmail.com>
2021-12-29 17:10:05 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(num1, num2){
+    let res = num1 + num2
+    return res
+}
+
+function subtract(num1, num2) {
+    let res = num1 - num2
+    return res
+}
+
+function multiply(num1, num2) {
+    let res = num1 * num2
+    return res
+}
+
+function divide(num1, num2) {
+    let res = num1 / num2
+    return res
+}
+
+function increment(n) {
+   n++;
+   return n
+}
+function decrement(n) {
+    n--;
+    return n
+}
+
+function makeInt(n) {
+   return parseInt(n,10);
+   
+}
+
+function preserveDecimal(n) {
+   return parseFloat(n)
+}
